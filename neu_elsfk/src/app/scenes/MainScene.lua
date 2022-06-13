7514bdad20e94fb4ca09de24a47c70570db3fa22
xingxingdongxi <1137175805@qq.com>
2022-06-13 22:04:01 +0800

    俄罗斯方块

--- neu_elsfk/src/app/scenes/MainScene.lua
+++ neu_elsfk/src/app/scenes/MainScene.lua
@@ -1,28 +1,74 @@
-require("app.Common")
+require("app/scenes/Common")
 
-local Scene=require"app.Scene"
-local Block=require"app.Block"
+local Scene=require"app/scenes/Scene"
+local Block=require"app/scenes/Block"
+local eventDispatcher = cc.Director:getInstance():getEventDispatcher()
 
 local MainScene = class("MainScene", function()
     return display.newScene("MainScene")
 end)
 
-local score=0
-local speedLevel=1
+
+function MainScene:yinXiao(path)
+        audio.loadFile(path, function ()
+            audio.playEffect(path, false)
+        end)
+end
+
 
 
 function MainScene:ctor()
+    self:ini()
 
 end
 
+function MainScene:ini()
+
+    cc.UserDefault:getInstance():setStringForKey("Score" ,0)
+end
+function MainScene:level(score)
+    if tonumber(score)<= 90000 then
+        return 1
+    end
+    return 9
+end
 function MainScene:onEnter()
 
     self.scene=Scene.new(self)
     self.b =Block.new(self.scene,1)
     self.b:Place()
     self:processInput()
+
+    self:Gen()
+
+    local Tick =function()
+        if self.pauseGame then
+            return
+        end
+        if not self.b:Move(0, -1) then
+            self:Gen()
+        else
+            self.b:Clear()
+
+            while self.scene:CheckAndSweep() > 0 do
+                self.scene:Shift()
+
+            end
+            self.b:Place()
+        end
+        print(cc.UserDefault:getInstance():getStringForKey("Score"))
+    end
+    cc.Director:getInstance():getScheduler():scheduleScriptFunc(Tick,1-self:level(cc.UserDefault:getInstance():getStringForKey("Score"))*0.1, false)
 end
 
+function MainScene:Gen()
+
+    self.b = Block.new(self.scene, RandomStyle() )
+    if not self.b:Place() then
+        --GameOver
+        self.scene:Clear()
+    end
+end
 function MainScene:processInput()
 
     local listener= cc.EventListenerKeyboard:create()
@@ -37,7 +83,10 @@ function MainScene:processInput()
         keyState[keyCode]=nil
         ---w
         if keyCode==146 then
-            self.b:Rotate()
+            self.b:zRotate()
+            ---e
+         elseif keyCode==128 then
+                self.b:yRotate()
         ---p
         elseif keyCode==139 then
             self.pauseGame =not self.pauseGame
@@ -49,9 +98,12 @@ function MainScene:processInput()
 
     local inputTick =function()
         for keyCode,v in pairs(keyState)do
-            ---s
+            ---s  下落
             if keyCode==142 then
                 self.b:Move(0,-1)
+            ---q  加速下落
+            elseif keyCode==140 then
+                self.b:Move(0,-2)
             ---a
             elseif keyCode==124 then
                 self.b:Move(-1,0)
@@ -59,9 +111,12 @@ function MainScene:processInput()
             elseif keyCode==127 then
                 self.b:Move(1,0)
             end
-        end
+            end
     end
+
     cc.Director:getInstance():getScheduler():scheduleScriptFunc(inputTick,0.1,false)
+
+
 end
 
 function MainScene:onExit()
