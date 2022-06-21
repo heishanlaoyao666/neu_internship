4baaa6d3a3781ab8087ae1294969f3a67b3e53be
3515819488 <3515819488@qq.com>
2022-06-21 22:39:31 +0800

    主界面未完成
    123

--- empty/src/app/scenes/MainScene.lua
+++ empty/src/app/scenes/MainScene.lua
@@ -5,6 +5,8 @@ end)
 
 function MainScene:ctor()
 
+    self:createMiddleMiddlePanel()
+
     --开始游戏按钮
     local images = {
         normal = "ui/hall/battle/Button-Battle_Mode.png",
@@ -26,7 +28,7 @@ function MainScene:ctor()
 
     NewGameBtn:addTouchEventListener(function(sender, eventType)
 	 	if eventType == ccui.TouchEventType.ended then
-	 		local ABtn = import("app/scenes/GameView/GameScene"):new()
+	 		local ABtn = import("app.scenes.GameScene"):new()
             display.replaceScene(ABtn,"turnOffTiles",0.5)
             print(transform)
 	 	end
@@ -34,10 +36,39 @@ function MainScene:ctor()
 
     self:addChild(NewGameBtn, 4)
 
+    self:createMiddleBottomPanel()
+
+
+end
+--bg-battle_interface.png
+function MainScene:createMiddleMiddlePanel()
+    local width ,height  =display.width,display.top
+    local settingLayer = ccui.Layout:create()
+    settingLayer:setBackGroundImage("ui/hall/battle/bg-battle_interface.png")
+    settingLayer:setContentSize(width,height)
+    settingLayer:setPosition(width*0.5,height*0.5)
+    settingLayer:setAnchorPoint(0.5,0.5)
+    settingLayer:addTo(self)
 
 end
 
-function MainScene:createMiddleMiddlePanel()
+function MainScene:createMiddleBottomPanel()
+    local width,height = display.width,display.top
+    local menuLayer = ccui.Layout:create()
+    menuLayer:setContentSize(width,height)
+    menuLayer:setPosition(width*0.5,0)
+    menuLayer:setAnchorPoint(0.5,0.5)
+    menuLayer:addTo(self)
+
+    local shopBtn = ccui.Button:create(
+        "ui/hall/bottom-tab/tab-unselected-left.png",
+        "ui/hall/bottom-tab/tab-selected.png",
+        "ui/hall/bottom-tab/tab-unselected-left.png"
+    )
+    shopBtn:setAnchorPoint(1,0)
+    shopBtn:setScale(2/3)
+    shopBtn:pos(width*0.5,height*0.5)
+    shopBtn:addTo(menuLayer)
     
 end
 
