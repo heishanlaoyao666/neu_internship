618cde8724a113b64856c20a2823f226250bbaff
3515819488 <106378242+3515819488@users.noreply.github.com>
2022-07-18 21:03:55 +0800

    7.18

--- empty/src/app/scenes/GameView/GameScene.lua
+++ empty/src/app/scenes/GameView/GameScene.lua
@@ -2,6 +2,10 @@
     GameSceneScene.lua
     战斗场景
 ]]
+local Music = require("app/data/Music")
+local SettingMusic = require("src/app/scenes/SettingMusic")
+local EventDef = require("app.def.EventDef.lua")
+local EventManager = require("app/manager/EventManager.lua")
 local GameScene = class("GameScene", function()
     return display.newScene("GameScene")
 end)
@@ -34,6 +38,248 @@ function GameScene:update(dt)
     self.playView_:update(dt)
 end
 function GameScene:onEnter()
+    EventManager:regListener(EventDef.ID.SCORE_SHOW_1, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.SCORESHOW[1], function ()
+                audio.playEffect(Music.SCORESHOW[1])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.SCORESHOW[1], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+    EventManager:regListener(EventDef.ID.SCORE_SHOW_2, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.SCORESHOW[2], function ()
+                audio.playEffect(Music.SCORESHOW[2])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.SCORESHOW[2], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+    EventManager:regListener(EventDef.ID.SCORE_SHOW_3, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.SCORESHOW[3], function ()
+                audio.playEffect(Music.SCORESHOW[3])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.SCORESHOW[3], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+    EventManager:regListener(EventDef.ID.INTO_GAME, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[1], function ()
+                audio.playEffect(Music.INGAME[1])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[1], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+    EventManager:regListener(EventDef.ID.SELECT_BOSS, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[2], function ()
+                audio.playEffect(Music.INGAME[2])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[2], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+    EventManager:regListener(EventDef.ID.CONFIRM_BOSS, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[3], function ()
+                audio.playEffect(Music.INGAME[3])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[3], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+    EventManager:regListener(EventDef.ID.BOSS_SHOW, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[4], function ()
+                audio.playEffect(Music.INGAME[4])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[4], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+    EventManager:regListener(EventDef.ID.TOWER_BUILD, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[5], function ()
+                audio.playEffect(Music.INGAME[5])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[5], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+    EventManager:regListener(EventDef.ID.TOWER_COMPOSE, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[6], function ()
+                audio.playEffect(Music.INGAME[6])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[6], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+    EventManager:regListener(EventDef.ID.TOWER_ATK, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[7], function ()
+                audio.playEffect(Music.INGAME[7])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[7], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+    EventManager:regListener(EventDef.ID.TOWER_ATK_HIT, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[8], function ()
+                audio.playEffect(Music.INGAME[8])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[8], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+    EventManager:regListener(EventDef.ID.WIN, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[9], function ()
+                audio.playEffect(Music.INGAME[9])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[9], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+
+    EventManager:regListener(EventDef.ID.GET_AWRADS, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[10], function ()
+                audio.playEffect(Music.INGAME[10])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[10], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+    EventManager:regListener(EventDef.ID.LOSE, self, function()
+        --音效
+        local MusicOn = SettingMusic:isMusic1()
+        print(MusicOn)
+        if MusicOn == true then
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[11], function ()
+                audio.playEffect(Music.INGAME[11])
+            end)
+        else
+            local audio = require("framework.audio")
+            audio.loadFile(Music.INGAME[11], function ()
+                audio.stopEffect()
+            end)
+        end
+    end)
+
+
+
+
 end
 
 function GameScene:onExit()
